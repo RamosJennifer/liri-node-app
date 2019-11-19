04f62dc07abf70830458c15d449e7987f3ce51fe
RamosJennifer <jenniferramos4858@gmail.com>
2019-11-18 19:34:41 -0600

    adding functionality to liri.js file

--- liri.js
+++ liri.js
@@ -1,1 +1,64 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var keys = require("./keys.js");
+var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
+
+
+function UserInputs (userOptions, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
+        break;
+    case 'spotify-this-song':
+        break;
+    case 'movie-this':
+        showMovieInfo(inputParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+
+    }
+}
+//function for concert info
+
+
+//function for music info: spotify
+
+
+
+//function for movie info: OMDB
+
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  } else {
+    movieName += nodeArgs[i];
+
+  }
+}
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Release Year: " + response.data.Year);
+  })
+// do-what-it-says
+
+
+//function fo reading out of random.txt file
